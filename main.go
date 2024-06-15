c40d2206912024e5ccb727524668e2aaecbc0b17
Rammbloor <rampant458@gmail.com>
2024-06-15 18:48:07 +0300

    Book

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ func main() {
 	fmt.Println("Cписок функций:\n" +
 		"ShowAll - Показывает все книги в хранилище\n" +
 		"AddBook - Внести данные про новую книгу\n" +
-		"UpdateBook - Обновить по названию книги\n" +
-		"DeleteBook - Удалить книгу по названию\n" +
+		"UpdateBook - Обновить книгу\n" +
+		"DeleteBook - Удалить книгу\n" +
 		"Введите исполняемую функцию!")
 
 	sc := bufio.NewScanner(os.Stdin)
